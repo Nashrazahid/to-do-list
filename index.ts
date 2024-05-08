b9fc9b518d1f0f1352a11b7fca65ef42905c2077
nashrazahid <nashrazahid5588@gmail.com>
2024-05-08 23:22:35 +0500

    created to do list

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let tudos=[]
+let condition = true
+
+while(condition){
+
+
+let addtask = await inquirer.prompt(
+    [
+        {
+         name:"todo",
+        type:"input",
+        message:"what do you want to add in your todos?"
+        },
+        {
+            name:"addmore",
+            type:"confirm",
+            message:"do you want to add more?",
+            default: "false"
+         }
+    ]
+)
+tudos.push(addtask.todo)
+condition=addtask.addmore
+console.log(tudos)
+}
